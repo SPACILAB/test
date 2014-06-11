ba11b839d0e2b1b074a5e8a29e9566819cfc9b8b
wenqianggit <260401959@qq.com>
2014-06-11 15:35:40 +0800

    Ver.1.2b
    It shows the area of VolumeOfTaper.

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+float VolumeOfTaper( float radius, float height )
+{
+    return 3.1416f * radius * radius * height / 3.0f;
+}
+
 int main(void)
 {
-    printf("Hello git!\n");
-    printf("Hello World!\n");
+    printf( "Hello git!\n" );
+    printf( "The volume of taper is %f\n",
+            VolumeOfTaper( 10.0f, 5.0f ) );
     return 0;
 }
 
